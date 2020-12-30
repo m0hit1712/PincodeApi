ad91d8c6e094785315615adb615dc76feaad9f59
m0hit1712 <mohit.patidar1503@gmail.com>
2020-12-30 07:58:36 +0530

    added csv file for pincodes from the official government website

--- api/views.py
+++ api/views.py
@@ -7,35 +7,31 @@ import json
 
 # Create your views here.
 
+
+def find_in_file(state, district, taluk):
+    filename = "./data/pincodes.json"
+    with open(filename) as file:
+        json_file = json.load(file)
+        for item in json_file:
+            if item['stateName'].lower() == state.lower():
+                if item['districtName'].lower() == district.lower():
+                    if item['taluk'].lower() == taluk.lower():
+                        pincode = item['pincode']
+                        return pincode
+    return None
+
+
 class GetPinCode(APIView):
-        def get(self, request, state, district, taluk):
-                self.state = state.replace("_"," ").lower()
-                self.district = district.replace("_"," ").lower()
-                self.taluk = taluk.replace("_"," ").lower()
-                pincode = self.return_pincode()
-                return Response({"pincode": pincode})
+    def get(self, request, state, district, taluk):
+        state = state.replace("_", " ").lower()
+        district = district.replace("_", " ").lower()
+        taluk = taluk.replace("_", " ").lower()
+        pincode = find_in_file(state, district, taluk)
+        return Response({"pincode": pincode})
 
-        def post(self, request, state, district, taluk):
-                self.state = state.replace("_"," ").lower()
-                self.district = district.replace("_"," ").lower()
-                self.taluk = taluk.replace("_"," ").lower()
-                pincode = self.return_pincode()
-                return Response({"pincode": pincode})
-                
-        def return_pincode(self):
-                filename = "./data/pincodes.json"
-                with open(filename) as file:
-                        json_file = json.load(file)
-                        for item in json_file:
-                                if item['stateName'].lower() == self.state.lower():
-                                        if item['districtName'].lower() == self.district.lower():
-                                                if item['taluk'].lower() == self.taluk.lower():
-                                                        pincode = item['pincode']
-                                                        return pincode
-                return None
-
-
-
-
-
-
+    def post(self, request, state, district, taluk):
+        state = state.replace("_", " ").lower()
+        district = district.replace("_", " ").lower()
+        taluk = taluk.replace("_", " ").lower()
+        pincode = find_in_file(state, district, taluk)
+        return Response({"pincode": pincode})
