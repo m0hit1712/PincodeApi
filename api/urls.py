b9dd3368f01437dca58c31fced906eb6816d8c07
m0hit1712 <mohit.patidar1503@gmail.com>
2021-01-11 18:21:15 +0530

    other urls for address api

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
         path('getpincode/<str:state>/<str:district>/<str:taluk>', views.GetPinCode.as_view(), name="pincode_url"),
+        path('getstates/<str:country>', views.GetDistricts.as_view(), name="state_url"),
+        path('getdistricts/<str:state>', views.GetStates.as_view(), name="district_url"),
+        path('gettaluks/<str:district>', views.GetTaluk.as_view(), name="taluk_url"),
 ]
 
 
--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import csv
 import json
 
 # Create your views here.
 
 
-def find_in_file(state, district, taluk):
+def find_in_json_file(state, district, taluk):
     filename = "./data/pincodes.json"
     with open(filename) as file:
         json_file = json.load(file)
@@ -20,18 +21,63 @@ def find_in_file(state, district, taluk):
                         return pincode
     return None
 
+def find_in_csv_file(state, district, taluk):
+    filename = "./data/pincodes_small.csv"
+    with open(filename, newline='') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if state == row["statename"] or state == row["circlename"]:
+                if district == row["DistrictName"] or district == row["regionname"]:
+                    if taluk == row["divisionname"] or taluk == row["Taluk"]:
+                        pincode = row["pincode"]
+                        return pincode
+    return None
 
 class GetPinCode(APIView):
     def get(self, request, state, district, taluk):
         state = state.replace("_", " ").lower()
         district = district.replace("_", " ").lower()
         taluk = taluk.replace("_", " ").lower()
-        pincode = find_in_file(state, district, taluk)
+        pincode = find_in_json_file(state, district, taluk)
+        if not pincode:
+            find_in_csv_file(state, district, taluk)
         return Response({"pincode": pincode})
 
     def post(self, request, state, district, taluk):
         state = state.replace("_", " ").lower()
         district = district.replace("_", " ").lower()
         taluk = taluk.replace("_", " ").lower()
-        pincode = find_in_file(state, district, taluk)
+        pincode = find_in_json_file(state, district, taluk)
+        if not pincode:
+            find_in_csv_file(state, district, taluk)
+
         return Response({"pincode": pincode})
+
+class GetDistricts(APIView):
+    def get(self, request, state):
+        state = state.replace("_", " ").lower()
+
+    def post(self, request, state):
+        state = state.replace("_", " ").lower()
+
+class GetStates(APIView):
+    def get(self, request, country):
+        country = country.replace("_", " ").lower()
+
+    def post(self, request, country):
+        country = country.replace("_", " ").lower()
+
+class GetTaluk(APIView):
+    def get(self, request, district):
+        district = district.replace("_", " ").lower()
+
+    def post(self, request, district):
+        district = district.replace("_", " ").lower()
+
+
+
+
+
+
+
+
